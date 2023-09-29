aa5dd3f13f64f1f688147d75cded71578c71d437
Stanislaw Skalkovich <stas.skalkovich@gmail.com>
2023-09-29 17:43:36 +0300

    2

---
+++ lab7/lab7/lab7/Controllers/BSTUController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lab7.Controllers;
+
+public class BSTUController:Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        
+        return View("~/Views/BSTUIndex.cshtml");
+    }
+}
