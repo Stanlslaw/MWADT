be543e2f101a83bd7c28a186134eb37ddb6f0ef6
Stanislaw Skalkovich <stas.skalkovich@gmail.com>
2023-10-23 19:58:17 +0300

    lab2b and lab 8 client update

---
+++ lab2/lab2b/lab2b/Controllers/TAResearchController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lab5b.Controllers;
+
+[Route("it")]
+public class TAResearchController:Controller
+{
+    [HttpGet]
+    [Route("{num:int}/{str:maxlength(32)}")]
+    public async Task<string> M04(int num,string str)
+    {
+        return $"{Request.Method}:M04:/{num}/{str}";
+    }
+
+    [HttpGet]
+    [HttpPost]
+    [Route("{b:bool}/{letters:alpha}")]
+    public async Task<string> M05(bool b, string letters)
+    {
+        return $"{Request.Method}:M05:/{b}/{letters}";
+    }
+
+    [HttpGet]
+    [HttpDelete]
+    [Route("{f:float}/{str:minlength(2):maxlength(5)}")]
+    public async Task<string> M06(float f, string str)
+    {
+        return $"{Request.Method}:M06:/{f}/{str}";
+    }
+    [HttpPut]
+    [Route("{letters:alpha:minlength(3):maxlength(4)}/{n:int:range(100,200)}")]
+    public async Task<string> M07(int? n, string? letters)
+    {
+        return $"{Request.Method}:M07:/{letters}/{n}";
+    }
+    [HttpPost]
+    [Route("{mail:regex(^\\S+@\\S+\\.\\S+$)}")]
+    public async Task<string> M08(string? mail)
+    {
+        return $"{Request.Method}:M08:/{mail}";
+    }
+
+}
---
+++ lab2/lab2b/lab2b/Controllers/TMResearchController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lab5a.Controllers;
+
+// [Route("{controller}")]
+// [Route("V2/{controller}")]
+// [Route("V3/{controller}/X")]
+public class MResearchController : Controller
+{
+    [Route("/")]
+    [Route("{controller}/M01/1")]
+    [Route("{controller}/M01")]
+    [Route("{controller}")]
+    [Route("V2/{controller}/M01")]
+    [Route("V3/{controller}/X/M01")]
+    [HttpGet]
+    public string M01()
+    {
+        return "GET:M01";
+    }
+    [Route("V2")]
+    [Route("V2/{controller}/M02")]
+    [Route("{controller}/M02")]
+    [Route("V3/{controller}/X/M02")]
+    [HttpGet]
+    public string M02()
+    {
+        return "GET:M02";
+    }
+    [Route("V3")]
+    [Route("V3/{controller}/X/")]
+    [Route("V3/{controller}/X/M03")]
+    [HttpGet]
+    public string M03()
+    {
+        return "GET:M03";
+    }
+    [Route("{controller}/MXX")]
+    [HttpGet]
+    public string MXX()
+    {
+        return "MXX";
+    }
+}
